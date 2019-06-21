40ec54c9ac7264fef4a348963fba638c0100116f
Tasuku Ohki <tasukuoki3@gmail.com>
2019-06-21 23:28:44 +0900

    URLを若干修正

--- sidebar_mypage.php
+++ sidebar_mypage.php
@@ -1,8 +1,8 @@
-    <section id="sidebar">
-        <a href="registProduct.php">対局結果を記録する</a>
-        <a href="tranSale.php">対戦履歴を検索する</a>
-        <a href="profEdit.php">プロフィール編集</a>
-        <a href="passEdit.php">パスワード変更</a>
-        <a href="withdraw.php">退会</a>
-    </section>
-    
+<section id="sidebar">
+    <a href="registProduct.php">対局結果を記録する</a>
+    <a href="index.php">対戦履歴を検索する</a>
+    <a href="profEdit.php">プロフィール編集</a>
+    <a href="passEdit.php">パスワード変更</a>
+    <a href="withdraw.php">退会</a>
+</section>
+

3fc703a92668b366a7c970ea15317cbe44c12b2d
Tasuku Ohki <tasukuoki3@gmail.com>
2019-07-10 23:56:37 +0900

    データ再編集時の画面遷移直後のフォーム非活性化解除処理追加

--- mokusuu.js
+++ mokusuu.js
@@ -5,7 +5,10 @@ $(function() {
     //ラジオボタンで「目数差」が入力されたら数値が入力できる形式にする
     //それまでは数値入力欄はdisabled（非活性）にしておく
 
-    //再編集時に、フォームに数値が最初から入力されていた場合、即非活性化解除をする（調整中）
+    //フォームに数値が最初から入力されていた場合(再編集)、即非活性化解除をする
+    if($('input[name="g_winHow_moku"]:text').val() != "" ) {
+        $('.js-disabled-form').prop('disabled', false);
+    }
 
     //ラジオボタンが切り替わった時「目数差」に指定されてるか確認するイベントをセット
     $('input[name="g_winHow"]:radio').change(function() {
@@ -20,4 +23,5 @@ $(function() {
             $('input[name="g_winHow_moku"]:text').val("");
         }
     });
+
 });

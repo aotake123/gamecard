4f3116f3f2e2aaf8df75688192be4f71a6853b1b
Tasuku Ohki <tasukuoki3@gmail.com>
2019-07-14 03:09:47 +0900

    管理者アカウントのログイン時にパスワード変更とプロフ編集が不要の為メニューから削除

--- sidebar_mypage.php
+++ sidebar_mypage.php
@@ -1,8 +1,14 @@
 <section id="sidebar">
     <a href="registProduct.php">対局結果を記録する</a>
     <a href="index.php">対戦履歴を検索する</a>
+    <?php
+    if($_SESSION['user_id'] != 17){
+    ?>
     <a href="profEdit.php">プロフィール編集</a>
     <a href="passEdit.php">パスワード変更</a>
     <a href="withdraw.php">退会</a>
+    <?php
+    }
+    ?>
 </section>
 
